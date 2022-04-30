a46bab7ea3ed7d4e424d6d0d68e96aae331e81e3
mariecreel <nick.creel@protonmail.com>
2022-04-30 12:26:25 -0400

    yarn add yargs, draft leetdex setup

---
+++ leetdex.mjs
@@ -0,0 +1,5 @@
+import * as cmd from "cmd/index.js"
+// pokédex API requires specific format for user agent
+// see https://pokedevs.gitbook.io/pokedex/reference
+// TODO: get version number from package.json?
+const ua = "leetdex (https://github.com/mariecreel/leetdex, 1.0.0)"
